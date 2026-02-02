2fae17654fb0ca4aea7234e1852d4f6b728998c3
nguyenquangmai <nguyenquangmai249@gmail.com>
2026-02-02 07:21:21 +0700

    1-9-13

---
+++ LeetCode Practice/PalindromeNumber9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class PalindromeNumber9
+    {
+        public static void Display()
+        {
+            Console.WriteLine(Solution(123));
+            Console.WriteLine(Solution(121));
+            Console.WriteLine(Solution(-121));
+        }
+
+        private static bool Solution(int x)
+        {
+            int res = 0;
+            int temp = x;
+
+            while (temp > 0)
+            {
+                res = (res * 10) + (temp % 10);
+                temp /= 10;
+            }
+
+            return x == res;
+        }
+    }
+}
---
+++ LeetCode Practice/TwoSum1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class TwoSum1
+    {
+        public static void Display()
+        {
+            int[] nums1 = { 2, 7, 11, 15 };
+            int[] nums2 = { 3, 2, 4 };
+            int[] nums3 = { 3, 3 };
+
+            int[] res = { 0, 0 };
+
+            res = Solution(nums1, 9);
+            Console.WriteLine(res[0] + "\t" + res[1]);
+
+            res = Solution(nums2, 6);
+            Console.WriteLine(res[0] + "\t" + res[1]);
+
+            res = Solution(nums3, 6);
+            Console.WriteLine(res[0] + "\t" + res[1]);
+        }
+
+        private static int[] Solution(int[] nums, int target)
+        {
+            int[] res = new int[2] { 0, 0 };
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        res[0] = i;
+                        res[1] = j;
+                        return res;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private static int[] Solution1ms(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int temp = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                temp = target - nums[i];
+
+                if (dic.ContainsKey(temp))
+                {
+                    return new int[] { dic[temp], i };
+                }
+
+                if (!dic.ContainsKey(temp))
+                {
+                    dic[nums[i]] = i;
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
